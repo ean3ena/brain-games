933857b6dba9b6f78b0db92611770ea391dbc55c
Andrey Eryomenko <ean3ena@yandex.ru>
2024-06-25 01:44:17 +0300

    add algorithm of choice the game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,22 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.askName();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(
+                "Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "0 - Exit");
+        System.out.print("Your choice: ");
+        int gameNumber = scanner.nextInt();
+
+        if (gameNumber != 0) {
+            Cli.askName();
+        }
+
+        scanner.close();
     }
 }
