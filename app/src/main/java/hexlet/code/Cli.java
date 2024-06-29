cb01b82e02a6657d55f82d4b0c6c0133d0c0f6f2
Andrey Eryomenko <ean3ena@yandex.ru>
2024-06-29 23:47:29 +0300

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,13 +3,17 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void askName() {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("May I have your name? ");
+    public static String greet(Scanner scanner) {
+
+        String firstGreeting = "\nWelcome to the Brain Games!\n"
+                                + "May I have your name? ";
+        System.out.print(firstGreeting);
+
         String userName = scanner.next();
+
         System.out.println("Hello, " + userName + "!");
 
-        scanner.close();
+        return userName;
     }
 }
