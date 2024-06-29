9a080e286e270a5596beeecf7c56fea87280392b
Andrey Eryomenko <ean3ena@yandex.ru>
2024-06-29 23:47:45 +0300

    add option to select game 'Even'

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,18 +3,32 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(
-                "Please enter the game number and press Enter.\n" +
-                "1 - Greet\n" +
-                "0 - Exit");
-        System.out.print("Your choice: ");
+        // Выводим список игр с предложением выбрать игру
+        String gameList = "Please enter the game number and press Enter.\n"
+                        + "1 - Greet\n"
+                        + "2 - Even\n"
+                        + "0 - Exit\n"
+                        + "Your choice: ";
+        System.out.print(gameList);
+
+        // Пользователь вводит номер игры
         int gameNumber = scanner.nextInt();
 
-        if (gameNumber != 0) {
-            Cli.askName();
+        // Номер игры должен быть из списка, кроме 0
+        if (gameNumber > 0 && gameNumber < 3) {
+
+            // Приветствуем игрока и возвращаем его имя
+            String userName = Cli.greet(scanner);
+
+            if (gameNumber == 2) {
+                // Игра "Проверка на чётность"
+                Even.startGame(scanner, userName);
+            }
         }
 
         scanner.close();
