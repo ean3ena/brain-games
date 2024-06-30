d91a031a644c27732314a06cc16f6a5aee488714
Andrey Eryomenko <ean3ena@yandex.ru>
2024-06-30 23:49:09 +0300

    refactor

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,31 +7,39 @@ public class Engine {
     }
 
     public static int getRandomNumber() {
-        return (int)(Math.random() * 100);
+        // Диапазон генерации случайных чисел
+        int min = 1;
+        int max = 100;
+
+        int randomNumber = (int)(min + Math.random() * max);
+        return randomNumber;
     }
 
-    public static void showQuestion(String expression) {
+    public static void requestPlayerAnswer(String expression) {
         System.out.println("Question: " + expression);
-    }
-
-    public static void showQuestion(int expression) {
-        showQuestion(Integer.toString(expression));
-    }
-
-    public static void requestAnswer() {
         System.out.print("Your answer: ");
+    }
+
+    public static void requestPlayerAnswer(int expression) {
+        requestPlayerAnswer(Integer.toString(expression));
+    }
+
+    public static boolean checkPlayerAnswer(String userName, String answer, String expectedAnswer) {
+        if (answer.equals(expectedAnswer)) {
+            notifyCorrectAnswer();
+            return true;
+        } else {
+            notifyWrongAnswer(answer, expectedAnswer, userName);
+            return false;
+        }
+    }
+
+    public static boolean checkPlayerAnswer(String userName, int answer, int expectedAnswer) {
+        return checkPlayerAnswer(userName, Integer.toString(answer), Integer.toString(expectedAnswer));
     }
 
     public static void notifyCorrectAnswer() {
         System.out.println("Correct!");
-    }
-
-    public static boolean isCorrectAnswer(String answer, String expectedAnswer) {
-        return answer.equals(expectedAnswer);
-    }
-
-    public static boolean isCorrectAnswer(int answer, int expectedAnswer) {
-        return answer == expectedAnswer;
     }
 
     public static void notifyWrongAnswer(String answer, String expectedAnswer, String userName) {
@@ -42,13 +50,7 @@ public class Engine {
         System.out.println(wrongAnswer);
     }
 
-    public static void notifyWrongAnswer(int answer, int expectedAnswer, String userName) {
-        notifyWrongAnswer(Integer.toString(answer), Integer.toString(expectedAnswer), userName);
-    }
-
-    public static void showCongratulation(int attemptCount, String userName) {
-        if (attemptCount == 0) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
+    public static void showCongratulation(String userName) {
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
